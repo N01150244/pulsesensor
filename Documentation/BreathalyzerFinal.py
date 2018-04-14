0942cbe7745f4e8f360ffe6a7f205330da5a9357
N01150244 <32016580+N01150244@users.noreply.github.com>
2018-04-14 02:46:48 +0000

    Update BreathalyzerFinal.py
    - Changed the wrapper to pyrebase
    - integrated database functionality to store/archive results

--- Documentation/BreathalyzerFinal.py
+++ Documentation/BreathalyzerFinal.py
@@ -1,12 +1,13 @@
-from firebase import firebase
-firebase = firebase.FirebaseApplication('https://breathalyzer.firebaseio.com/')
+import pyrebase
 import time
 import serial
 import RPi.GPIO as GPIO
-GPIO.VERSION
+#GPIO.setup(11,GPIO.OUT)
+#GPIO.VERSION
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(16, GPIO.IN)
+
 ser=serial.Serial("/dev/ttyACM1", 9600)
 ser.baudrate=9600
 def blink(pin):
@@ -16,11 +17,8 @@ def blink(pin):
       GPIO.output(pin, LOW)
       time.sleep(1)
       return
-
-#GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11,GPIO.OUT)
-from smbus import SMBus  
-
+ 
+from smbus import SMBus 
 bus = SMBus(1)
 
 def read_ain(i):    
@@ -31,8 +29,20 @@ def read_ain(i):
     bus.read_byte(0x48)   
     return bus.read_byte(0x48)
 
-applicant = raw_input("Enter your username:")
-      
+config = {
+    "apiKey": "AIzaSyBMu4FC-4kiSRXdmLOQgFZOUMG15JBHG-Y",
+    "authDomain": "breathalyzer.firebaseapp.com",
+    "databaseURL": "https://breathalyzer.firebaseio.com",
+    "projectId": "breathalyzer",
+    "storageBucket": "breathalyzer.appspot.com",
+    "messagingSenderId": "179506469389"
+  }
+
+firebase = pyrebase.initialize_app(config)
+
+
+applicant = input("Enter your username:")
+     
 i = 0
 count = 0
 j = 0
@@ -40,17 +50,20 @@ while(i<5):
     heartrate1 = read_ain(1)
     alcohol = read_ain(2)*0.001
     read_ser=ser.readline()
-    
-    print "\n----Testing----\n"
+    hr2 = read_ser.decode('utf-8')
+    print ("\n----Testing----\n")
     print("HeartRate Sensor: {0:.0f} BPM".format(heartrate1))
     print ("Alcohol Reading: {0:.2f}%\n".format(alcohol))
-    print ("Wireless Heart Rate : %s\n" %read_ser)
+    print ("Wireless Heart Rate : %s\n" %hr2)
     time.sleep(3)
     i+=1
-    #heartrate2 = int(read_ser)
+    
    
-   
-result = firebase.patch('https://breathalyzer.firebaseio.com/user/'+applicant,{'Heart Rate1':heartrate1}) 
-result2 = firebase.patch('https://breathalyzer.firebaseio.com/user/'+applicant,{'Alcohol Reading':alcohol})
-result3 = firebase.patch('https://breathalyzer.firebaseio.com/user/'+applicant,{'Heart Rate2':read_ser})     
-
+db = firebase.database()
+result = db.child("user").child(applicant).update({'Heart Rate1':heartrate1})
+result2 = db.child("user").child(applicant).update({'Alcohol Reading':alcohol})
+result3 = db.child("user").child(applicant).update({'Heart Rate2':hr2})
+result4 = db.child("user").child(applicant).child("Past Results HR1").push(heartrate1)
+result5 = db.child("user").child(applicant).child("Past Results Alcohol Reading").push(alcohol)
+result6 = db.child("user").child(applicant).child("Past Results HR2").push(hr2)
+    
